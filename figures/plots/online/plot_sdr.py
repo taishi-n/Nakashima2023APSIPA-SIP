c528b739f7ffc712b31df60c74e6707c7f677d35
taishi-n <taishi.m.and.p@gmail.com>
2023-06-19 13:40:22 +0900

    fix: plots

--- figures/plots/online/plot_sdr.py
+++ figures/plots/online/plot_sdr.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import pathlib
+import argparse
 
 sns.mpl.use("pgf")
 sns.set(
@@ -9,25 +11,29 @@ sns.set(
         "axes.grid": True,
         "axes.titlepad": 4,
         "axes.labelpad": 2,
-        # "font.size": 9,
+        "axes.labelsize": "medium",
+        "font.size": 8,
         "font.family": "serif",
+        # "font.family": "sans-serif",
         "font.serif": "cmr10",
-        # "font.sans-serif": "Noto Sans JP",
+        "font.sans-serif": "Hiragino Sans",
         "grid.alpha": 0.8,
         "grid.linestyle": "--",
         "grid.linewidth": 0.5,
-        "legend.fontsize": "x-small",
+        "legend.fontsize": "xx-small",
         "legend.frameon": True,
         "legend.framealpha": 0.9,
-        "legend.title_fontsize": "small",
-        "lines.linewidth": 0.8,
-        "lines.markersize": 2,
+        "legend.title_fontsize": "x-small",
+        "lines.linewidth": 0.75,
+        "lines.markersize": 1,
         "pdf.fonttype": 42,
         "savefig.pad_inches": 0.05,
         "savefig.transparent": True,
         "text.usetex": True,
         "xtick.direction": "in",
         "ytick.direction": "in",
+        "xtick.labelsize": "small",
+        "ytick.labelsize": "small",
     },
     style="ticks",
 )
@@ -35,38 +41,39 @@ sns.set_palette("tab10")
 
 # この2個だけはrcParamsで指定してもseaborn内で上書きされる
 plot_kws = {
-    # "mew": 0.0,
     "mec": None,
 }
 
 # figure size
 mm = 1 / 25.4  # inch to mm
 aspect = 1.61
-fig_w = 75 * mm
+fig_w = 50 * mm
 fig_h = fig_w / aspect
 
 
-df = pd.read_csv("bsseval_all.csv", index_col=False)
+parser = argparse.ArgumentParser(prog="PlotSDR", description="Plot segmental SI-SDR improvements.")
+parser.add_argument("csvpath")
+args = parser.parse_args()
+csvpath = pathlib.Path(args.csvpath)
+df = pd.read_csv(csvpath, index_col=False)
 drop = [
-    "fft.win",
     "bss.ref_mic",
     "bss.n_iter",
-    "fft.len",
-    "fft.hop",
     "bss.demix_update",
     "SDR_est",
     "SDR_mix",
 ]
+
 df.drop(columns=drop, inplace=True)
 
 by = [
     "frame",
-    "process",
     "channel",
+    "process",
     "bss.source_model",
-    "bss.forget",
     "bss.f_limit",
-    "mixdir_name",
+    "bss.forget",
+    "fft.len",
 ]
 
 group = df.groupby(by=by, as_index=False).mean().reset_index()
@@ -75,58 +82,64 @@ group = df.groupby(by=by, as_index=False).mean().reset_index()
 new_cols = {
     "frame": "Time (s)",
     "bss.source_model": "Source model",
-    "bss.forget": "Forgetting factor",
     "bss.f_limit": "Frequency band limitation",
-    "mixdir_name": "SFI",
     "SDR_imp": "SI-SDRi (dB)",
+    "bss.forget": "Forgetting factor",
 }
 group.rename(columns=new_cols, errors="raise", inplace=True)
 print(group)
 
 # replace
-group.replace({"online": "Naive", "online-reset": "Reset", "online-sfi": "SFI", "online-rot": "CT"}, inplace=True)
+methods = {
+    "online-rot": "RT-cov",
+    "online-sfi": "RT-obs",
+    "online-reset": "Reset",
+    "online": "Naive",
+}
+group.replace(methods, inplace=True)
+print(group)
 
-for model in group[new_cols["bss.source_model"]].unique():
-    for freq in group[new_cols["bss.f_limit"]].unique():
-        for alpha in group[new_cols["bss.forget"]].unique():
-            print(model, freq, alpha)
-            queries = [
-                f"`Source model` == '{model}'",
-                f"`{new_cols['bss.forget']}` == {alpha}",
-                f"`{new_cols['bss.f_limit']}` == {freq}",
-            ]
-            d = group.query(" and ".join(queries))
-            grid = sns.relplot(
-                data=d,
-                x=new_cols["frame"],
-                y=new_cols["SDR_imp"],
-                hue="process",
-                hue_order=["CT", "SFI", "Reset", "Naive"],
-                style="process",
-                markers=True,
-                dashes=False,
-                kind="line",
-                ci=None,
-                height=fig_h,
-                aspect=aspect,
-                facet_kws={
-                    "legend_out": False,
-                    "despine": False,
-                    "xlim": (0, 30),
-                    # "ylim": (-10, 30),
-                },
-                **plot_kws,
-            )
+print(group[new_cols["bss.forget"]].unique())
+for fftlen in group["fft.len"].unique():
+    for alpha in group[new_cols["bss.forget"]].unique():
+        for model in group[new_cols["bss.source_model"]].unique():
+            for freq in group[new_cols["bss.f_limit"]].unique():
+                print(model, freq, alpha)
+                queries = [
+                    f"`{new_cols['bss.forget']}` == {alpha}",
+                    f"`{new_cols['bss.source_model']}` == '{model}'",
+                    f"`{new_cols['bss.f_limit']}` == {freq}",
+                ]
+                d = group.query(" and ".join(queries))
+                grid = sns.relplot(
+                    data=d,
+                    x=new_cols["frame"],
+                    y=new_cols["SDR_imp"],
+                    # row="channel",
+                    hue="process",
+                    hue_order=list(methods.values()),
+                    style="process",
+                    markers=True,
+                    dashes=False,
+                    kind="line",
+                    errorbar=None,
+                    height=fig_h,
+                    aspect=aspect,
+                    facet_kws={"legend_out": True, "despine": False, "xlim": (0, 60), "ylim": (-15, 25)},
+                    **plot_kws,
+                )
 
-            # adjust
-            grid.fig.axes[0].axvline(x=15, color="k", linewidth=0.75, linestyle="--")
-            grid.fig.tight_layout()  # (left, bottom, right, top)
-            grid.fig.subplots_adjust(**dict(left=0.18, bottom=0.20, right=0.97, top=0.97))
-            # lg = grid.fig.axes[0].legend_
-            # lg.set_title(None)
-            # lg.texts[0].set_text("w/ SFI")
-            # lg.texts[1].set_text("w/o SFI")
+                # remove title in legend
+                grid.fig.legends[0].remove()
+                handles, labels = grid.ax.get_legend_handles_labels()
+                grid.fig.axes[0].legend(handles, labels, loc="lower center", bbox_to_anchor=(0.5, 1), ncol=4)
 
-            grid.fig.savefig(f"{model}_{freq}_{str(alpha)[2:]}.pdf")
-            plt.clf()
-            plt.close()
+                # adjust
+                grid.fig.axes[0].axvline(x=30, color="k", linewidth=0.75, linestyle="--")
+                grid.fig.tight_layout()  # (left, bottom, right, top)
+                grid.fig.subplots_adjust(**dict(left=0.18, bottom=0.22, right=0.91, top=0.88))
+
+                alpha_fmt = f"{alpha:.2f}"[2:]
+                grid.fig.savefig(csvpath.parent / f"{model}_{freq}_fft{fftlen}_{alpha_fmt}.pdf")
+                plt.clf()
+                plt.close()
